fc46b921ea4976970656d1a4c98d97a313e37995
xAT47x <154707970+xAT47x@users.noreply.github.com>
2023-12-24 00:18:23 +0200

    Update FUNC.h

--- FUNC.h
+++ FUNC.h
@@ -6,11 +6,7 @@
 //here in the .h file we're going to write the prototypes of our functions
 //those are examples from a previous project i did
 //obviously we're going to delete them 
-void create_account(clients *ptr);
-u16 check_password(u32 x);
-void delay(int y);
-void set_pasid(clients *ptr);
-void transaction(clients *ptr,u64 targeted_id,u16 amount);
+int login(void);
 
 
 

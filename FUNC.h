c4832373ed45b3ea055790008dfd7f1c6383cb70
xAT47x <154707970+xAT47x@users.noreply.github.com>
2023-12-25 17:21:03 +0200

    Update FUNC.h

--- FUNC.h
+++ FUNC.h
@@ -18,7 +18,7 @@ typedef struct
 //here in the .h file we're going to write the prototypes of our functions
 
 int login(User *ptr);
-int count_accounts(const char *filename);
+int count_accounts();
 accounts * load(int accnum);
 int trans(accounts*accounts_infile,int n,int *sender,int *receiver);
 int save(accounts *account_list,int n);

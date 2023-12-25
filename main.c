9c395d1ac50eced3b98ecd9bd31fb650a1cd3452
RONIN <131418749+o-ronin-o@users.noreply.github.com>
2023-12-25 14:03:34 +0200

    Update main.c

--- main.c
+++ main.c
@@ -23,37 +23,49 @@ that's a branch lolololol
 */
 #include <stdio.h>
 #include "FUNC.h"
-#include "FUNC.c"
-typedef struct
-{
-    long long account_number;
-    char name[100];
-    char email[100];
-    double balance;
-    long long mobile;
-    char date_opened[20];
-} accounts;
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>  // Include this for the toupper function
 
 void main(void)
 {
-	int choice1 = 0;
-	//we need to write the script for the login here then we procceed to provide features
-	while(1)
-	{
-		
-		//you're required to complete your task presentation in the line below then procceed to write the script in the switch 
-		printf("\nwhat do you want to do?\nL load account data.\nS Search.\nE Exit system.\n");
-				fflush(stdin);
-				scanf(" %c" , &choice1);
-				choice1 = toupper(choice1);
-				//switch case for the choices of the admin
-				switch(choice1)
-				{
-					case 'L' :
-							//now you can write the code here
-							
-					case 'S' :
-							// EXTRA EXAMPLE
-				}
-	}
+    int check = 0;
+    int choice1 = 0;
+    User s1[100];  // Assuming you meant to declare an array of User
+    // While loop for login
+    while (1)
+    {
+        check = login(s1);
+        if (check == 1)
+        {
+            printf("Welcome back\n");
+            printf("\nWhat do you want to do?\nL Load account data.\nS Search.\nE Exit system.\n");
+
+            fflush(stdin);
+            scanf(" %c", &choice1);
+            choice1 = toupper(choice1);
+
+            // Switch case for the choices of the admin
+            switch (choice1)
+            {
+                case 'L':
+                    printf("Test is valid\n");
+                    // Now you can write the code here
+                    break;
+                case 'S':
+                    // EXTRA EXAMPLE
+                    break;
+                case 'E':
+                    // Add code for exiting the system
+                    break;
+                default:
+                    printf("Invalid choice\n");
+            }
+        }
+        else
+        {
+            printf("Exiting system\n");
+            break;
+        }
+    }
 }

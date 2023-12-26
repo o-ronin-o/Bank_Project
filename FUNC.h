5c7b9f7e5f79a84877e5b4ec903f595defee6b89
ARAHMAN04 <154708801+ARAHMAN04@users.noreply.github.com>
2023-12-26 14:01:23 +0200

    deposit FUNC.h

--- FUNC.h
+++ FUNC.h
@@ -26,7 +26,7 @@ int trans(accounts*accounts_infile,int n,int *sender,int *receiver);
 int save(accounts *account_list,int n);
 void withdraw(accounts *ptr, int acc_no);
 void advanced_search(accounts *ptr,int acc_no );
-
+void deposit(accounts *ptr, int acc_no);
 
 
 

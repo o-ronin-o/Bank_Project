2fa617841578dd6024a726ecbf53930678bc5a6d
RONIN <131418749+o-ronin-o@users.noreply.github.com>
2023-12-27 14:49:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,72 +1,92 @@
-
-/*
-NAME: Omar.W.Al-Qattan 				   || 	        last update: December 21,2023
-NAME: Abdallah.A.Na'eem 			   ||		last update:
-NAME: Abdelrahman.A.Abdo(7asal eh) 	           || 		last update:
-NAME: Tarek.M.El-Sayed 				   || 		last update:
-NAME: Muhammed.Y.Abu-Tabl 			   || 		last update:
-
-Please note that you HAVE TO write comments consistently to intrepret and clarify your code for the other team members
-so we coud easily track possible errors
-*/
-
-
 #include <stdio.h>
 #include "FUNC.h"
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>  // Include this for the toupper function
+#include <ctype.h>
 
 int main(void)
 {
     int check = 0;
     int choice1 = 0;
     User s1[100];  // Assuming you meant to declare an array of User
+
     // While loop for login
     while (1)
     {
         check = login(s1);
 
-		int number_of_accounts=count_accounts();
-		accounts *accounts_infile = load(number_of_accounts);
+        int number_of_accounts = count_accounts();
+        accounts *accounts_infile = load(number_of_accounts);
+
         if (check == 1)
         {
             printf("Welcome back\n");
-            printf("\nWhat do you want to do?\nL Load account data.\nS Search.\nE Exit system.\nT Transfer balance\n");
-
-
-
+            printf("\nMenu:\n");
+            printf("1. ADD\n");
+            printf("2. DELETE\n");
+            printf("3. MODIFY\n");
+            printf("4. SEARCH\n");
+            printf("5. ADVANCED SEARCH\n");
+            printf("6. WITHDRAW\n");
+            printf("7. DEPOSIT\n");
+            printf("8. TRANSFER\n");
+            printf("9. REPORT\n");
+            printf("10. PRINT\n");
+            printf("11. QUIT\n");
 
             fflush(stdin);
-            scanf(" %c", &choice1);
-            choice1 = toupper(choice1);
+            scanf("%d", &choice1);
 
             // Switch case for the choices of the admin
             switch (choice1)
             {
-                case 'L':
-                    printf("Test is valid\n");
-                    // Now you can write the code here
+                case 1:
+                    // Call the add function here
                     break;
-                case 'S':
-                    // EXTRA EXAMPLE
+                case 2:
+                    delete_account(accounts_infile, &number_of_accounts);
                     break;
-                case 'E':
+                case 3:
+                    modify(accounts_infile, number_of_accounts);
+                    break;
+                case 4:
+                    // Call the search function here
+                    break;
+                case 5:
+                    advanced_search(accounts_infile, number_of_accounts);
+                    break;
+                case 6:
+                    withdraw(accounts_infile, number_of_accounts);
+                    break;
+                case 7:
+                    deposit(accounts_infile, number_of_accounts);
+                    break;
+                case 8:
+                    trans(accounts_infile, number_of_accounts);
+                    break;
+                case 9:
+                    // Call the report function here
+                    break;
+                case 10:
+                    print_sorted(accounts_infile, number_of_accounts);
+                    break;
+                case 11:
                     // Add code for exiting the system
                     break;
-
-		    case 'T':
-			{
-				trans(accounts_infile,number_of_accounts);
-				break;
-			}
-			default:
-			{
-				printf("went to the default");
-				break;
-			}
-		    }
-		return 0;
-}
+                default:
+                    printf("Invalid option\n");
+                    break;
+            }
+        }
+        else if (check == 0)
+        {
+            printf("Invalid username or password\n");
+        }
+        else
+        {
+            printf("Error: File not found.\n");
+        }
     }
-	}
+
+    return 0;
+}
